f5ab2d78aa116a3945fb48479508b8eb731ddf7c
Insafsuz <in7afsuz@gmail.com>
2023-09-18 11:36:02 +0300

    fix bugs

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function mobileSlider() {
   if (window.innerWidth > 767) {
     slider.dataset.mobile = 'false'
 
-    if (slider.classList.contains('swiper-container-initialized')) {
+    if (slider.classList.contains('swiper-initialized')) {
       mySwiper.destroy()
     }
   }
